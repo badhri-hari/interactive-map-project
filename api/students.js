2429ad8ff0160fd6dedb9deb7117f6e64e0dea5a
badhri-hari <average.runner216@gmail.com>
2024-07-03 20:31:39 +0530

    Initial commit.

---
+++ api/students.js
@@ -0,0 +1,68 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
+const apiKey = process.env.API_KEY;
+
+let cachedClient = null;
+
+const connectToDatabase = async () => {
+  if (
+    cachedClient &&
+    cachedClient.topology &&
+    cachedClient.topology.isConnected()
+  ) {
+    return cachedClient;
+  }
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  cachedClient = client;
+  return client;
+};
+
+export default async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
+
+  const requestApiKey = req.headers["x-api-key"];
+  if (!requestApiKey || requestApiKey !== apiKey) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  try {
+    const client = await connectToDatabase();
+    const db = client.db(dbName);
+    const collection = db.collection("School_Transport");
+
+    let query = {};
+    if (req.query.Name) {
+      query.Name = { $regex: new RegExp(`^${req.query.Name}`, "i") };
+    }
+    if (req.query.StudentId) {
+      query.StudentId = { $regex: new RegExp(req.query.StudentId, "i") };
+    }
+    if (req.query.Address) {
+      query.Address = { $regex: new RegExp(req.query.Address, "i") };
+    }
+    if (req.query.Area) {
+      query.Area = { $regex: new RegExp(`^${req.query.Area}`, "i") };
+    }
+
+    const students = await collection.find(query).toArray();
+    res.status(200).json(students);
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: err.message });
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,183 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
+
+export default function App() {
+  const [students, setStudents] = useState([]);
+  const [search, setSearch] = useState({ Name: "", StudentId: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const firstStudentRef = useRef(null);
+
+  const handleSearch = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("/api/students", {
+        params: search,
+        headers: {
+          "x-api-key": process.env.REACT_APP_API_KEY,
+        },
+      });
+      const sortedStudents = response.data.sort((a, b) =>
+        a.Name.localeCompare(b.Name)
+      );
+      setStudents(sortedStudents);
+    } catch (error) {
+      console.error("Error fetching student data:", error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const formatStudentId = (value) => {
+    const cleanValue = value.replace(/[^a-zA-Z0-9]/g, "");
+    let formattedValue = "";
+
+    if (cleanValue.length > 0) {
+      formattedValue += cleanValue.substring(0, 2).toUpperCase();
+    }
+    if (cleanValue.length > 2) {
+      formattedValue += "-" + cleanValue.substring(2, 4);
+    }
+    if (cleanValue.length > 4) {
+      formattedValue += "-" + cleanValue.substring(4, 7);
+    }
+
+    return formattedValue;
+  };
+
+  useEffect(() => {
+    if (students.length > 0) {
+      firstStudentRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [students]);
+
+  return (
+    <>
+      <div className="app-container">
+        <div className="intro-container">
+          <h1>School Transport Students Map</h1>
+        </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Student ID"
+            className="search-input"
+            value={search.StudentId}
+            onChange={(e) =>
+              setSearch({
+                ...search,
+                StudentId: formatStudentId(e.target.value),
+              })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Student Name"
+            className="search-input"
+            value={search.Name}
+            onChange={(e) => setSearch({ ...search, Name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            className="search-input"
+            onChange={(e) => setSearch({ ...search, Address: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Area"
+            className="search-input"
+            onChange={(e) => setSearch({ ...search, Area: e.target.value })}
+          />
+          <br />
+          <button className="search-button" onClick={handleSearch}>
+            Search
+          </button>
+          {error && (
+            <div className="error-message">
+              There was an error. Please try again.
+            </div>
+          )}
+        </div>
+        {loading && (
+          <div className="loading-overlay">
+            <div className="loading-container">
+              <div className="loading-text">Loading map...</div>
+            </div>
+          </div>
+        )}
+        <MapContainer
+          center={[12.99520434846565, 80.25563741504301]}
+          zoom={10}
+          className="map-container"
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {students.map(
+            (student, index) =>
+              student.latitude &&
+              student.longitude && (
+                <Marker
+                  key={index}
+                  position={[student.latitude, student.longitude]}
+                >
+                  <Popup>
+                    <div className="popup-intro-container">
+                      <p className="popup-text">
+                        <strong>
+                          {student.Name} ({student.StudentId})
+                        </strong>
+                      </p>
+                      <div className="popup-text student-grade">
+                        {student.Grade}
+                      </div>
+                    </div>
+                    <div className="popup-text-container">
+                      <p className="popup-text">
+                        <strong>
+                          <u>Route Number:</u>
+                        </strong>{" "}
+                        {student.RouteNo}
+                      </p>
+                      <p className="popup-text">
+                        <strong>
+                          <u>Address:</u>
+                        </strong>{" "}
+                        {student.Address}
+                      </p>
+                      <p className="popup-text">
+                        <strong>
+                          <u>Pickup Time:</u>
+                        </strong>{" "}
+                        {student.Pickup}
+                      </p>
+                      <p className="popup-text">
+                        <strong>
+                          <u>Drop Time:</u>
+                        </strong>{" "}
+                        {student.Drop}
+                      </p>
+                    </div>
+                  </Popup>
+                </Marker>
+              )
+          )}
+        </MapContainer>
+      </div>
+    </>
+  );
+}
